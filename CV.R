bdcb6a878d21d361886d55d297842a3e1a6ec296
zsh400 <zsh48@msstate.edu>
2021-12-09 11:52:13 -0800

    Initial commit

---
+++ CV.R
@@ -0,0 +1,78 @@
+---
+  title: "Curriculum Vitae"
+description: |
+  Latin for "course of life"
+output: distill::distill_article
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = FALSE)
+```
+
+<center>
+
+# ZY'KEYAH S. HORN
+
+Parker-Hannifin
+
+[zykeyahhorn\@yahoo.com](mailto:zykeyahhorn@yahoo.com){.email}
+
+</center>
+
+------------------------------------------------------------------------
+
+*It is my mission to learn and inspire with honesty and integrity as I walk the path God has set for me. I strive to be a leader, dedicated to leading the masses on why we believe, not just focused on what we believe.*
+
+------------------------------------------------------------------------
+
+# Education
+
+**Mississippi State University, Adkerson School of Accountancy** Starkville, MS---*In Progress*
+
+*Master of Professional Accountancy Candidate*
+
+**Mississippi State University, Adkerson School of Accountancy** Starkville, MS
+
+*Bachelor of Accountancy with a minor in Pre-Law*
+
+·        Magna Cum Laude
+
+**Meridian Community College,** Meridian, MS
+
+(*DUAL ENROLLMENT STUDENT)*
+
+June 2016 -- May 2018
+
+·        24 hours
+
+·        GPA 4.0
+
+# Experience
+
+**Parker-Hannifin,** Cleveland, Ohio---*Accounting Leadership Development Associate*
+
+July 2021-- Current
+
+·        Responsible for the submission of weekly payroll report submissions for two sites
+
+·        Process all payments and apply them against invoices
+
+**Mississippi State Forestry,** Starkville, MS---*Accounting Assistant*
+
+July 2018-- April 2021
+
+·        Analyze and prepare monthly financial reports
+
+·        Execute invoices from vendors
+
+·        Scan and file all paper documents into database
+
+# Activities and Associations
+
+**National Association of Black Accountants­-** *President (2020- 2021)*
+
+**Zeta Phi Beta Sorority, Inc-** *Treasurer (2020-2021)*
+
+**Brickfire Mentoring Program-** *Mentor (2019-2021)*
+
+**Community Service--** *Future Pride and Ms. Smith's Educational Services Volunteer (2018-Current)*
